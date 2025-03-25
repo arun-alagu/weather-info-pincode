23efde3440d3c3c010f7898cb9e400ebe0e1b2aa
arun-alagu <ahesharun@gmail.com>
2025-03-26 00:07:46 +0530

    add index for tables

--- src/main/java/com/arun/app/models/PincodeLocation.java
+++ src/main/java/com/arun/app/models/PincodeLocation.java
@@ -3,12 +3,20 @@ package com.arun.app.models;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity(name = "pincode_locations")
+@Entity
+@Table(
+		name = "pincode_locations",
+		indexes = {
+				@Index(name ="index_pincode", columnList ="pincode")
+		}
+		)
 public class PincodeLocation extends BaseModel implements Serializable {
 	private String pincode;
 	private String name;
--- src/main/java/com/arun/app/models/WeatherData.java
+++ src/main/java/com/arun/app/models/WeatherData.java
@@ -5,6 +5,8 @@ import java.time.LocalDate;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +14,11 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(
+		indexes = {
+				@Index(name = "index_latitude_longitude_date", columnList = "latitude, longitude, date")
+		}
+		)
 public class WeatherData extends BaseModel implements Serializable{
 	private Double latitude;
 	private Double longitude;
